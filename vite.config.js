385119cc18f2c124a318d2830280981f304f7826
Laurynas Dauksa <68541403+skirtingas@users.noreply.github.com>
2026-04-12 15:49:34 +0300

    trying to fix build

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
+import path from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import { cloudflare } from '@cloudflare/vite-plugin'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools(), cloudflare()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(process.cwd(), 'src')
     },
   },
 })
